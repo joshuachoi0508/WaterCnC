4ab394b601d31fe77779cc06afdaeb5ba12ce72a
Josh Choi <joshuachoi0508@gmail.com>
2019-03-23 14:16:36 -0700

    first commit

---
+++ frontend/util/user_api_util.js
@@ -0,0 +1,27 @@
+export const fetchUser = userId => (
+    $.ajax({
+        method: 'GET',
+        url: `api/users/${userId}`,
+    })
+);
+
+export const updateUser = user => (
+    $.ajax({
+        method: 'PATCH',
+        url: `api/users/${user.id}`,
+        dataType: 'json',
+        contentType: false,
+        processData: false,
+        data: user,
+    })
+);
+
+export const fetchAllUsers = username => (
+    $.ajax({
+        method: 'GET',
+        url: 'api/users',
+        data: {
+            username
+        }
+    })
+);
