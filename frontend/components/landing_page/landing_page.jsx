da12913c589991ce7a5edac2b602257c21bd8533
Josh Choi <joshuachoi0508@gmail.com>
2019-03-24 11:19:16 -0700

    Landing navbar complete

--- frontend/components/landing_page/landing_page.jsx
+++ frontend/components/landing_page/landing_page.jsx
@@ -6,9 +6,17 @@ render(){
     return(
       <div id="landing">
         <div className="nav-bar">
-          <p onClick={() => this.props.openModal('signup')}>
-            Signup
-          </p>
+          <div className="nav-bar-left">
+            <img className="nav-bar-logo" src={window.images.navbar_logo} />
+          </div>
+          <div className="nav-bar-right">
+            <p className="nav-right-item" onClick={() => this.props.openModal('login')}>
+              login
+            </p>
+            <p className="nav-right-item" onClick={() => this.props.openModal('signup')}>
+              Signup
+            </p>
+          </div>
         </div>
         <div id="search">
           <div className="content">
