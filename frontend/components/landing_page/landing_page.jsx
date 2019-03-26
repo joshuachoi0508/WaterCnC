6a9bbbc7449207b2999170063f56c4751e48c6ef
Josh Choi <joshuachoi0508@gmail.com>
2019-03-26 16:59:01 -0700

    Guest drop down closes with click outside of dropdown

--- frontend/components/landing_page/landing_page.jsx
+++ frontend/components/landing_page/landing_page.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { DateRangePicker } from 'react-dates';
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
@@ -8,11 +8,8 @@ import {
   getLatLng,
 } from 'react-places-autocomplete';
 import NavBar from './nav_bar';
-import moment from 'moment';
 
-const today = moment();
-
-class LandingPage extends React.Component {
+class LandingPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -22,14 +19,18 @@ class LandingPage extends React.Component {
       focusedInput: null,
       openDropdown: 'hidden',
       numGuests: 1,
-      numAdults: 1,
-      numChildren: 0,
-      numInfants: 0,
-      address: ''
+      address: '',
+      renderGuest: 'num-guest-hide'
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSelect = this.handleSelect.bind(this);
+    this.renderGuests = this.renderGuests.bind(this);
+    this.disableGuests = this.disableGuests.bind(this);
+  }
+
+  componentDidMount(){
+    document.addEventListener('click', (e) => this.disableGuests(e));
   }
 
   handleChange(address) {
@@ -41,7 +42,38 @@ class LandingPage extends React.Component {
       .then(results => getLatLng(results[0]))
       .then(latLng => console.log('Success', latLng))
       .catch(error => console.error('Error', error));
-  };
+  }
+
+  renderGuestCount() {
+    if (this.state.numGuests > 1) {
+      return String(this.state.numGuests) + " guests";
+    } else {
+      return String(this.state.numGuests) + " guest";
+    }
+  }
+
+  disableGuests(e) {
+    debugger;
+    if ((e.target.id === "search" || e.target.className === "lets-go") && this.state.renderGuest === 'num-guest-show') {
+      this.setState({['renderGuest']: 'num-guest-hide'})
+    }
+  }
+
+  renderGuests() {
+    if (this.state.renderGuest === 'num-guest-show') {
+      this.setState({['renderGuest']: 'num-guest-hide'})
+    } else {
+      this.setState({['renderGuest']: 'num-guest-show'})
+    }
+  }
+
+  renderMinusButton() {
+    if (this.state.numGuests === 1) {
+      return (<button disabled={true} className="guest-count-button disabled">-</button>)
+    } else {
+      return (<button className="guest-count-button">-</button>)
+    }
+  }
 
   render(){
       return(
@@ -85,7 +117,7 @@ class LandingPage extends React.Component {
                                   cursor: 'pointer' ,
                                   padding: '10px'
                                 }
-                              : { 
+                                : { 
                                   backgroundColor: '#ffffff', 
                                   cursor: 'pointer',
                                   padding: '10px'
@@ -93,6 +125,7 @@ class LandingPage extends React.Component {
                             return (
                               <div
                                 onClick={(e) => this.handleChange(e.target.innerText)} 
+                                className="autocomplete-dropdown-container"
                               >
                                 <div
                                   {...getSuggestionItemProps(suggestion, {
@@ -133,9 +166,24 @@ class LandingPage extends React.Component {
                     endDatePlaceholderText="Check out"
                   />
                 </div>
-              </div>
-              <div id="search-bar">
-                <div className='search-input'>
+                <div className="guest-picker">
+                  <div className="splash-guest-text">
+                    <span>GUEST</span>
+                  </div>
+                  <div className="guest-botton-container">
+                    <button
+                      className="guest-button"
+                      onClick={this.renderGuests}
+                    >
+                      {this.renderGuestCount()}
+                    </button>
+                  </div>
+                  <div id={this.state.renderGuest}>
+                    <p>Guests</p>
+                    {this.renderMinusButton()}
+                          {this.state.numGuests}
+                    <button className="guest-count-button">+</button>
+                  </div>
                 </div>
               </div>
             </div>
